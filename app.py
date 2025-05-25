e5f0486e4fd1f63abe2531ae4adc4fc4c1e0b247
Moblei <moritz.bleines@gmail.com>
2025-05-25 12:22:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if uploaded_file:
         try:
             client = InferenceHTTPClient(
                 api_url="https://infer.roboflow.com",
-                api_key=st.secrets["ROBOFLOW_API_KEY"]
+                api_key=st.secrets["JVv6sT45apmdOuYNBbnE"]
             )
 
             result = client.run_workflow(
